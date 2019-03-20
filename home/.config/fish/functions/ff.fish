6cf454e2b5dbeaf0da69dd70ae1f9ff06be78fb7
Pascal Hertleif <killercup@gmail.com>
2019-03-20 10:46:04 +0100

    fix ffmpeg type

--- home/.config/fish/functions/ff.fish
+++ home/.config/fish/functions/ff.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/n3/dkk459k908lcmkzwcmq0tcv00000gn/T//fish.FPYxBp/ff.fish @ line 2
+# Defined in /var/folders/n3/dkk459k908lcmkzwcmq0tcv00000gn/T//fish.CeKyEK/ff.fish @ line 2
 function ff --description 'download a stream with ffmpeg'
-	ffmpeg -i $argv[1] -c copy (date +"%Y-%m-%dT%H-%m-%S")-$argv[2].mp4
+	ffmpeg -i $argv[1] -c copy (date +"%Y-%m-%dT%H-%M-%S")-$argv[2].mp4
 end
